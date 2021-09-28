a5d08a939ca43447dc756b94a8366e1444385689
juniorsolo <junior.solo@hotmail.com>
2021-09-27 22:11:03 -0300

    Removendo primeNg. Instalando bootstrap e jquery.

--- angular/barLamc-app/src/app/app.module.ts
+++ angular/barLamc-app/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {ButtonModule} from 'primeng/button';
+
 
 @NgModule({
   declarations: [
@@ -11,7 +11,6 @@ import {ButtonModule} from 'primeng/button';
   ],
   imports: [
     BrowserModule,
-    ButtonModule,
     AppRoutingModule
   ],
   providers: [],
