5e9bca89cc339483fb83c7a188007129d0ff3522
juniorsolo <junior.solo@hotmail.com>
2021-10-10 20:09:07 -0300

    Desenhando tela de cobrança.

--- angular/barLamc-app/src/app/cobrancas/cobrancas.component.ts
+++ angular/barLamc-app/src/app/cobrancas/cobrancas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faHandHoldingUsd } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-cobrancas',
@@ -6,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CobrancasComponent implements OnInit {
 
+  /**Icones */
+  iconeCobrancaTitulo = faHandHoldingUsd;
+
+  dataAtual = new Date();
+
+
   constructor() { }
 
   ngOnInit() {
+    console.log(this.dataAtual.getMonth()+1 );
   }
 
 }
