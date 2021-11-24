7a2cb2735b4261f219366b5a69c938e833765dd3
juniorsolo <junior.solo@hotmail.com>
2021-11-23 22:26:30 -0300

    Criando projeto bar para o back-end.

---
+++ bar/src/main/java/lamc/bar/controllers/IntegranteController.java
@@ -0,0 +1,7 @@
+package lamc.bar.controllers;
+
+
+
+public class IntegranteController {
+
+}
---
+++ bar/src/test/java/lamc/bar/BarApplicationTests.java
@@ -0,0 +1,13 @@
+package lamc.bar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
